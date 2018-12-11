5aadaf66039c04bb252f533e1e61643e27b8292b
marianna <mariannadudzinska@gmail.com>
2018-12-11 14:28:37 +0100

    sort consumptions by headers

--- src/main/webapp/app/entities/food/food.component.ts
+++ src/main/webapp/app/entities/food/food.component.ts
@@ -72,7 +72,7 @@ export class FoodComponent implements OnInit, OnDestroy {
         private principal: Principal,
         private userExtraService: UserExtraService
     ) {
-        this.sortedFoods = this.foods;
+        // this.sortedFoods = this.foods;
         this.sortedData = this.desserts.slice();
     }
 
@@ -253,6 +253,87 @@ export class FoodComponent implements OnInit, OnDestroy {
         });
     }
 
+    sortTable(n) {
+        let table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+        table = document.getElementById('nutrientVals');
+        switching = true;
+        dir = 'asc';
+        while (switching) {
+            switching = false;
+            rows = table.rows;
+            for (i = 1; i < (rows.length - 1); i++) {
+                shouldSwitch = false;
+                x = rows[i].getElementsByTagName('TD')[n];
+                y = rows[i + 1].getElementsByTagName('TD')[n];
+                if ((y.className === 'sortable' ) &&
+                    (x.className === 'sortable' )) {
+                    if (dir === 'asc') {
+                            if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                                shouldSwitch = true;
+                                break;
+                            }
+                    } else if (dir === 'desc') {
+                            if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+                                shouldSwitch = true;
+                                break;
+                            }
+                    }
+                }
+            }
+            if (shouldSwitch) {
+                rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+                switching = true;
+                switchcount ++;
+            } else {
+                if (switchcount === 0 && dir === 'asc') {
+                    dir = 'desc';
+                    switching = true;
+                }
+            }
+        }
+    }
+
+    sortTableVals(n) {
+        let table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+        table = document.getElementById('nutrientVals');
+        switching = true;
+        dir = 'asc';
+        while (switching) {
+            switching = false;
+            rows = table.rows;
+            for (i = 1; i < (rows.length - 1); i++) {
+                shouldSwitch = false;
+                x = rows[i].getElementsByTagName('TD')[n];
+                y = rows[i + 1].getElementsByTagName('TD')[n];
+                if (( y.className === 'count-me' || y.className === 'count-carbs' ||
+                    y.className === 'count-protein' || y.className === 'count-fats') &&
+                    (x.className === 'count-me' || x.className === 'count-carbs' ||
+                        x.className === 'count-protein' || x.className ===  'count-fats')) {
+                    if (dir === 'asc') {
+                        if (Number(x.innerHTML) > Number(y.innerHTML)) {
+                            shouldSwitch = true;
+                            break;
+                        }
+                    } else if (dir === 'desc') {
+                        if (Number(x.innerHTML) < Number(y.innerHTML)) {
+                            shouldSwitch = true;
+                            break;
+                        }
+                    }
+                }
+            }
+            if (shouldSwitch) {
+                rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+                switching = true;
+                switchcount ++;
+            } else {
+                if (switchcount === 0 && dir === 'asc') {
+                    dir = 'desc';
+                    switching = true;
+                }
+            }
+        }
+    }
     sortData(sort: Sort) {
         const data = this.desserts.slice();
         if (!sort.active || sort.direction === '') {
